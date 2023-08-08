d2329ff9e2d08442bb09316ce28a48ff67d33db6
Aleksandr Grigorenko <techgriss@gmail.com>
2023-08-08 21:03:30 +0300

    some routing fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,3 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import { BoardsPage } from '@/pages';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <BoardsPage />,
-  },
-]);
-
-export const App = () => <RouterProvider router={router} />;
+export const App = () => <BoardsPage />;

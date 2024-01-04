80ad3ae25967ee523dcc20ba992953ab2de13aa4
Aleksandr Grigorenko <techgriss@gmail.com>
2024-01-04 17:47:46 +0300

    boards list example

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ListBoardResponseItem } from 'anyboards-proto/gen/js/boards/boards_pb';
 import { BoardsClient } from 'anyboards-proto/gen/js/boards/BoardsServiceClientPb';
+import * as google_protobuf_empty_pb from 'google-protobuf/google/protobuf/empty_pb';
 import { ThemeProvider } from 'styled-components';
 
 import { App } from '@/app';
@@ -8,9 +10,17 @@ import { App } from '@/app';
 import { GlobalStyle, theme } from '@/shared';
 
 const connect = async () => {
-  const boardsClient = new BoardsClient('http://localhost:3000');
+  const boardsClient = new BoardsClient('http://localhost:2222');
 
-  console.log(boardsClient);
+  const boardsList = await boardsClient
+    .listBoards(new google_protobuf_empty_pb.Empty(), null)
+    .then((res) =>
+      res
+        .getItemList()
+        .map((board) => board.toObject() as ListBoardResponseItem.AsObject)
+    );
+
+  console.log(boardsList);
 };
 
 const container = document.getElementById('root') as HTMLElement;
